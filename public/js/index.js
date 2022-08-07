168a64af97e3ce4383bb382009f9271ee0f8afc1
AlvaroRK <adriano.rk06@gmail.com>
2022-08-06 23:29:11 -0300

    1.7 fetch agregado (corregir errores)

--- public/js/index.js
+++ public/js/index.js
@@ -14,135 +14,137 @@ const divi  = (a,b) => a / b;
 
 
 
-    /* ---DEPLOY PRODUCTS--- */
-
-
-    const cards = document.getElementById("cards");
-    const cartContainer = document.getElementById("items");
-    const emptyButton = document.getElementById("vaciarCarrito");
-    const cartCounter = document.getElementById("contadorCarrito")
-    const totalPrice = document.getElementById("precioTotal")
-
-    emptyButton.addEventListener("click", () => {
-      cart.length = 0
-      updateCart()
-    })
-
-let listProducts = [
-  {id:1 ,image:"./public/assets/img/Fernet 550X550.png" ,desc:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, eaque?",nombre: "Fernet Branca 750ml", precio: 1300, cantidad:1},
-  {id:2 ,image:"./public/assets/img/CocaCola 550X550.png",desc:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, eaque?",nombre: "Coca Cola 1.5Lts", precio: 190, cantidad:1},
-  {id:3 ,image:"./public/assets/img/Mate 550X550.png",desc:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, eaque?",nombre: "Mate", precio: 1400, cantidad:1},
-  {id:4 ,image:"./public/assets/img/YerbaMate 550X550.png",desc:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, eaque?",nombre: "Yerba mate", precio: 700, cantidad:1},
-  {id:5 ,image:"./public/assets/img/JorgitoChoco 550X550.png",desc:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, eaque?",nombre: "Alfajor Jorgito|chocolate", precio: 81, cantidad:1},
-  {id:6 ,image:"./public/assets/img/DonSatur 550X550.png",desc:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, eaque?",nombre: "Don Satur Dulce", precio: 120, cantidad:1},
-  {id:7 ,image:"./public/assets/img/PipasGirasol 550X550.png",desc:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, eaque?",nombre: "Pipas Girasol", precio: 50, cantidad:1},
-  {id:8 ,image:"./public/assets/img/Chocolinas 550X550.png",desc:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, eaque?",nombre: "Chocolinas", precio: 170, cantidad:1},
-  {id:9 ,image:"./public/assets/img/DulceDeLeche 550X550.png",desc:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, eaque?",nombre: "Dulce de leche", precio: 500, cantidad:1},
-  {id:10 ,image:"./public/assets/img/Tita 550X550.png",desc:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, eaque?",nombre: "Tita", precio: 75, cantidad:1},
-  {id:11 ,image:"./public/assets/img/Havanna 550X550.png",desc:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, eaque?",nombre: "Alfajor Havanna 12unids", precio: 2800, cantidad:1},
-  {id:12 ,image:"./public/assets/img/PicoDulce 550X550.png",desc:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, eaque?",nombre: "Pico Dulce", precio: 800, cantidad:1},
-];
 
 /* ---SECCION DEL CARRITO DE COMPRAS--- */
 
-let cart = [];
 
-document.addEventListener('DOMContentLoaded', () => {
-  if (localStorage.getItem('cart')){
-    cart = JSON.parse(localStorage.getItem('cart'))
-    updateCart()
-  }
-})
- 
-listProducts.forEach(producto => {
-  const div = document.createElement(`div`);
-  div.classList.add("card")
-  div.innerHTML = ` <img src="${producto.image}">
-                    <h4>${producto.nombre}</h4>
-                    <h3>${producto.desc}</h3>
-                    <p>$${producto.precio}</p>
-                    <button class="btnAddToCart" id="btn${producto.id}">Add to cart</button>
- `;
-  cards.appendChild(div)
 
-   const boton = document.getElementById(`btn${producto.id}`)
+    const pedirCatalogo = async () => {
+      try {
+        const response = await fetch('./public/catalogue.JSON')
+        const data = await response.json();
 
-  boton.addEventListener("click", () => {
-    addToCart(producto.id)
-  })
-});
 
-const addToCart = (prodId) => {
+        const cards = document.getElementById("cards");
+        const cartContainer = document.getElementById("items");
+        const emptyButton = document.getElementById("vaciarCarrito");
+        const cartCounter = document.getElementById("contadorCarrito")
+        const totalPrice = document.getElementById("precioTotal")
 
-  const existe = cart.some(prod => prod.id === prodId)
+        emptyButton.addEventListener("click", () => {
+          cart.length = 0;
+          updateCart();
+        })
 
-  if(existe){
-    const prod = cart.map (prod => {
-      (prod.id === prodId) ? prod.cantidad ++ : "";
-    })
-  } else {
-    const item = listProducts.find((prod) => prod.id === prodId);
-    cart.push(item)
-    const Toast = Swal.mixin({
-      toast: true,
-      position: 'top-end',
-      showConfirmButton: false,
-      timer: 1300,
-      didOpen: (toast) => {
-        toast.addEventListener('mouseenter', Swal.stopTimer)
-        toast.addEventListener('mouseleave', Swal.resumeTimer)
-      }
-    })
+        let cart = [];
+
+        document.addEventListener('DOMContentLoaded', () => {
+          if (localStorage.getItem('cart')){
+            cart = JSON.parse(localStorage.getItem('cart'));
+            updateCart()
+          }
+        })
+
+
+        data.forEach(producto => {
+          const div = document.createElement(`div`);
+          div.classList.add("card");
+          div.innerHTML = ` <img src="${producto.image}">
+                          <h4>${producto.nombre}</h4>
+                          <h3>${producto.desc}</h3>
+                          <p>$${producto.precio}</p>
+                          <button class="btnAddToCart" id="btn${producto.id}">Add to cart</button>`;
+          cards.appendChild(div)
     
-    Toast.fire({
-      icon: 'success',
-      title: `${item.nombre} agregado correctamente`
-    })
-  }
-  updateCart()
-}
+          const boton = document.getElementById(`btn${producto.id}`);
+    
+          boton.addEventListener("click", () => {
+            addToCart(producto.id);
+        });
+      });
 
-const updateCart = () => {
-  cartContainer.innerHTML = ""
+    listProducts = data;
 
-  cart.forEach((prod) => {
-    const div = document.createElement('div');
-    div.className = ('productoEnCarrito');
-    div.innerHTML = `
-      <p>${prod.nombre}</p>
-      <p>Price: ${prod.precio}</p>
-      <p>Amount: <span id="cantidad">${prod.cantidad}</span></p>
-      <button onclick="removeFromCart(${prod.id})" class="botonEliminar"><i class="fas fa-trash-alt"></button>`
-    cartContainer.appendChild(div)
+    const addToCart = (prodId) => {
 
-    localStorage.setItem("cart", JSON.stringify(cart))
-  })
-  cartCounter.innerText = cart.length
-  totalPrice.innerText = cart.reduce((acum , prod) => acum + prod.precio, 0)
-}
-
-const removeFromCart = (prodId) => {
-  const item = cart.find((prod) => prod.id === prodId)
-  const indice = cart.indexOf(item)
-  cart.splice(indice, 1)
-  localStorage.removeItem("cart", "item")
-  const Toast = Swal.mixin({
-    toast: true,
-    position: 'top-end',
-    showConfirmButton: false,
-    timer: 1300,
-    didOpen: (toast) => {
-      toast.addEventListener('mouseenter', Swal.stopTimer)
-      toast.addEventListener('mouseleave', Swal.resumeTimer)
+      const existe = cart.some(prod => prod.id === prodId)
+    
+      if(existe){
+        const prod = cart.map (prod => {
+          (prod.id === prodId) ? prod.cantidad ++ : "";
+        })
+      } else {
+        const item = listProducts.find((prod) => prod.id === prodId);
+        cart.push(item)
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 1300,
+          didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+          }
+        })
+        
+        Toast.fire({
+          icon: 'success',
+          title: `${item.nombre} agregado correctamente`
+        })
+      }
+      updateCart()
     }
-  })
-  
-  Toast.fire({
-    icon: 'error',
-    title: `${item.nombre} eliminado correctamente`
-  })
-  updateCart()
-}
+    
+    const updateCart = () => {
+      cartContainer.innerHTML = ""
+    
+      cart.forEach((prod) => {
+        const div = document.createElement('div');
+        div.className = ('productoEnCarrito');
+        div.innerHTML = `
+          <p>${prod.nombre}</p>
+          <p>Price: ${prod.precio}</p>
+          <p>Amount: <span id="cantidad">${prod.cantidad}</span></p>
+          <button onclick="removeFromCart(${prod.id})" class="botonEliminar"><i class="fas fa-trash-alt"></button>`
+        cartContainer.appendChild(div)
+    
+        localStorage.setItem("cart", JSON.stringify(cart))
+      })
+      cartCounter.innerText = cart.length
+      totalPrice.innerText = cart.reduce((acum , prod) => acum + prod.precio, 0)
+    }
+    
+    const removeFromCart = (prodId) => {
+      const item = cart.find((prod) => prod.id === prodId)
+      const indice = cart.indexOf(item)
+      cart.splice(indice, 1)
+      localStorage.removeItem("cart", "item")
+
+      const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 1300,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+      
+      Toast.fire({
+        icon: 'error',
+        title: `${item.nombre} eliminado correctamente`
+      })
+            updateCart()
+    }
+
+        
+      } catch (error) {
+        console.log(error);
+      }
+  }
+
+  pedirCatalogo();
 
 /* SECCION DE CONTACTO */
 
