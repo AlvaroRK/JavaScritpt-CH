0c9de4c5eaf0f8f5895350aea39350bcb8e9c5d0
AlvaroRK <adriano.rk06@gmail.com>
2022-08-13 23:51:08 -0300

    1.8 Entrega final

--- public/js/index.js
+++ public/js/index.js
@@ -19,7 +19,7 @@ const divi  = (a,b) => a / b;
 
 
 
-    const pedirCatalogo = async () => {
+    const requestCatalog = async () => {
       try {
         const response = await fetch('./public/catalogue.JSON')
         const data = await response.json();
@@ -32,43 +32,49 @@ const divi  = (a,b) => a / b;
         const totalPrice = document.getElementById("precioTotal")
 
         emptyButton.addEventListener("click", () => {
+          if (cart.length != 0){
+            const Toast = Swal.mixin({
+              toast: true,
+              position: 'top-end',
+              showConfirmButton: false,
+              timer: 1300,
+              didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+              }
+            })
+            Toast.fire({
+              icon: 'error',
+              title: `cart emptied correctly`
+            })
+          }
           cart.length = 0;
+          localStorage.removeItem("cart");
           updateCart();
         })
 
         let cart = [];
 
-        document.addEventListener('DOMContentLoaded', () => {
-          if (localStorage.getItem('cart')){
-            cart = JSON.parse(localStorage.getItem('cart'));
-            updateCart()
-          }
-        })
-
-
-        data.forEach(producto => {
+        data.forEach(product => {
           const div = document.createElement(`div`);
           div.classList.add("card");
-          div.innerHTML = ` <img src="${producto.image}">
-                          <h4>${producto.nombre}</h4>
-                          <h3>${producto.desc}</h3>
-                          <p>$${producto.precio}</p>
-                          <button class="btnAddToCart" id="btn${producto.id}">Add to cart</button>`;
+          div.innerHTML = ` <img src="${product.image}">
+                          <h4>${product.nombre}</h4>
+                          <h3>${product.desc}</h3>
+                          <p>$${product.precio}</p>
+                          <button class="btnAddToCart" id="btn${product.id}">Add to cart</button>`;
           cards.appendChild(div)
-    
-          const boton = document.getElementById(`btn${producto.id}`);
-    
+
+          const boton = document.getElementById(`btn${product.id}`);
           boton.addEventListener("click", () => {
-            addToCart(producto.id);
+            addToCart(product.id);
+          });
         });
-      });
 
     listProducts = data;
 
     const addToCart = (prodId) => {
-
       const existe = cart.some(prod => prod.id === prodId)
-    
       if(existe){
         const prod = cart.map (prod => {
           (prod.id === prodId) ? prod.cantidad ++ : "";
@@ -86,10 +92,9 @@ const divi  = (a,b) => a / b;
             toast.addEventListener('mouseleave', Swal.resumeTimer)
           }
         })
-        
         Toast.fire({
           icon: 'success',
-          title: `${item.nombre} agregado correctamente`
+          title: `${item.nombre} successfully added`
         })
       }
       updateCart()
@@ -97,21 +102,24 @@ const divi  = (a,b) => a / b;
     
     const updateCart = () => {
       cartContainer.innerHTML = ""
-    
       cart.forEach((prod) => {
+        let PrecioXCantidad = multi(prod.precio, prod.cantidad)
         const div = document.createElement('div');
         div.className = ('productoEnCarrito');
         div.innerHTML = `
           <p>${prod.nombre}</p>
-          <p>Price: ${prod.precio}</p>
+          <p>Price: ${PrecioXCantidad}</p>
           <p>Amount: <span id="cantidad">${prod.cantidad}</span></p>
-          <button onclick="removeFromCart(${prod.id})" class="botonEliminar"><i class="fas fa-trash-alt"></button>`
+          <button id="botonEliminar(${prod.id})" class="botonEliminar"><i class="fas fa-trash-alt"></button>`
         cartContainer.appendChild(div)
+
+        const botonEliminar = document.getElementById(`botonEliminar(${prod.id})`)
+        botonEliminar.addEventListener('click', removeFromCart)
     
         localStorage.setItem("cart", JSON.stringify(cart))
       })
       cartCounter.innerText = cart.length
-      totalPrice.innerText = cart.reduce((acum , prod) => acum + prod.precio, 0)
+      totalPrice.innerText = cart.reduce((acum , prod) => acum + prod.precio * prod.cantidad , 0)
     }
     
     const removeFromCart = (prodId) => {
@@ -133,18 +141,22 @@ const divi  = (a,b) => a / b;
       
       Toast.fire({
         icon: 'error',
-        title: `${item.nombre} eliminado correctamente`
+        title: `successfully removed`
       })
             updateCart()
     }
 
+      if (localStorage.getItem('cart')){
+        cart = JSON.parse(localStorage.getItem('cart'));
+        updateCart()
+      }
         
       } catch (error) {
         console.log(error);
       }
   }
 
-  pedirCatalogo();
+  requestCatalog();
 
 /* SECCION DE CONTACTO */
 
@@ -154,7 +166,7 @@ btnSubmit.addEventListener("click", () => {
   Swal.fire({
     toast: true,
     icon:'success',
-    title:'Gmail enviado correctamente',
+    title:'Gmail sent successfully',
     timer:1300,
     showConfirmButton: false,
     position:'top-end'
